1d8b5028a169d2ece749b156eebd148346560428
Стас <stas08072002@gmail.com>
2020-12-18 17:10:20 +0300

    Fix

--- Competencies/SelectFile.cs
+++ Competencies/SelectFile.cs
@@ -57,7 +57,7 @@ namespace Competencies
                     NameOfDevelopersFile.Text = "Загрузка...";
                     string xlPath = SelectFile.FileName;
                     _Excel.xlWorkDevelopers = _Excel.xlApp.Workbooks.Open(xlPath);
-                    _Excel.xlReferenceKo202 = (Excel.Worksheet)_Excel.xlWorkDevelopers.Sheets["Справка КО 20-2"];
+                    _Excel.xlReferenceKo202 = (Excel.Worksheet)_Excel.xlWorkDevelopers.Sheets[1];
                     NameOfDevelopersFile.Text = Path.GetFileNameWithoutExtension(xlPath);
                     Competencies.CreateDevelopersDic(_Excel.xlReferenceKo202);
                 }
